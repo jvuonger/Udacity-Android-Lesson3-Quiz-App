c28de09e7aa427e51014b26af65631147fc4cc47
James Vuong <jvuonger@gmail.com>
2016-09-11 19:22:59 -0400

    Toast + ID's

--- app/src/main/java/com/jamesvuong/quizapp/MainActivity.java
+++ app/src/main/java/com/jamesvuong/quizapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jamesvuong.quizapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkQuiz(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "Congrats, you got 10/10 answers right!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
